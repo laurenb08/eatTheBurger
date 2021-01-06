aceb1afcdb557a0a26b17b1d56dec175baf18709
Lauren Plenger <laurenb08@gmail.com>
2021-01-05 21:36:31 -0800

    updated schema files

--- schema.sql
+++ schema.sql
@@ -1,19 +1,25 @@
 -- Drops the task_saver_db if it already exists --
 DROP DATABASE IF EXISTS burgers_db;
-
 -- Create the database task_saver_db and specified it for use.
 CREATE DATABASE burgers_db;
-
 USE burgers_db;
-
 -- Create the table tasks.
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
   burger varchar(255) NOT NULL,
+  eaten BOOLEAN,
   PRIMARY KEY (id)
 );
-
 -- Insert a set of records.
-INSERT INTO burgers (burger) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger) VALUES ('Bacon Burger');
-INSERT INTO burgers (burger) VALUES ('Whiskey River BBQ Burger');
+INSERT INTO
+  burgers (burger, eaten)
+VALUES
+  ('Cheeseburger', false);
+INSERT INTO
+  burgers (burger, eaten)
+VALUES
+  ('Bacon Burger', false);
+INSERT INTO
+  burgers (burger, eaten)
+VALUES
+  ('Whiskey River BBQ Burger', false);
