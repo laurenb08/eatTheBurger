c234723dc309cae2b2b288b1991dee11a4b0e277
Lauren Plenger <laurenb08@gmail.com>
2021-01-05 22:21:52 -0800

    rearranged files and fixed bugs

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/", function (req, res) {
     connection.query("SELECT * FROM burgers;", function (err, data) {
         if (err) throw err;
 
-        res.redirect("/");
+        res.render("index", { burgers: data });
     });
 });
 
