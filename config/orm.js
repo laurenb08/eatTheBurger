58833d883b8dbda8a64dbc98306e7aacf836c93f
Lauren Plenger <laurenb08@gmail.com>
2021-01-08 13:48:55 -0800

    added methods to orm.js

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+var connection = require("../config/connection.js");
+
+function selectAll() {
+
+};
+
+function insertOne() {
+
+};
+
+function updateOne() {
+
+};
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
