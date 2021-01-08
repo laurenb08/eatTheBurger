6ba6b0a93d59f3dc143dde6fde45bc6e250cd4c9
Lauren Plenger <laurenb08@gmail.com>
2021-01-08 14:52:04 -0800

    changed schema.sql

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,6 @@
 var connection = require("../config/connection.js");
 
-function selectAll() {
 
-};
-
-function insertOne() {
-
-};
-
-function updateOne() {
-
-};
 
 // Export the orm object for the model (cat.js).
 module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
   burger varchar(255) NOT NULL,
-  eaten BOOLEAN,
+  eaten BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
