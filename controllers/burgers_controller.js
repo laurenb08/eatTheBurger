ab6be0dc81bbe48c65132fa6ad52f016a09a9051
Lauren Plenger <laurenb08@gmail.com>
2021-01-08 14:01:24 -0800

    added router to burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,7 @@
 var express = require("express");
+var router = express.Router();
 var connection = require("../models/burgers.js");
+
+
+// Export routes for server.js to use.
+module.exports = router;
