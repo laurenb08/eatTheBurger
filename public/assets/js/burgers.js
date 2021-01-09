a15288824db923845bac5709ca56afc828148623
Lauren Plenger <laurenb08@gmail.com>
2021-01-08 16:53:38 -0800

    added to burgers.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -19,4 +19,25 @@ $(function () {
         );
     })
 
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+    
+        var newBurger = {
+          burger: $("#ca").val().trim(),
+          eaten: $("[burger=eaten]:checked").val().trim()
+        };
+
+        // Send the POST request.
+    $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("added new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+    
 });
