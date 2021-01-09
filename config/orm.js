0ee65e497e9a88758cc13379f917cd28d7578daa
Lauren Plenger <laurenb08@gmail.com>
2021-01-08 16:36:38 -0800

    updating orm.js

--- config/orm.js
+++ config/orm.js
@@ -10,7 +10,27 @@ var orm = {
           cb(result);
         });
       },
+      create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the model (burgers.js).
 module.exports = orm;
