e2e53b9e38768637914258748580cb5a65637afa
Lauren Plenger <laurenb08@gmail.com>
2021-01-13 21:41:35 -0800

    adding heroku connections

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,24 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+let connection;
+require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Murphy212!",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "burgers_db"
+  })
+};
+
 
 // Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+connection.connect((err) => {
+  if (err) throw err;
   console.log("connected as id " + connection.threadId);
 });
 
